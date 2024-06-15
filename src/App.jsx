a749d733cfb20b8c07e0a2472bf78ef5350a6603
sudhir25464 <sudhir202118@gmail.com>
2024-06-15 20:22:48 +0530

    update color name text on UI

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,19 @@ import './App.css'
 function App() {
 
   const [color, setColor] = useState("olive");
+  const [colorName, setColorName] = useState();
   return (
     <div className='w-full h-screen duration-200'
 
-      style={{ backgroundColor: color }}>
+      style={{ backgroundColor: color }}> 
+      <h3 className='flex justify-center text-white py-8 text-5xl font-medium'>{color}</h3>
 
       <div className='fixed flex flex-wrap  justify-center inset-x-0 bottom-12'>
         <div className='flex felx-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded '>
 
-          <button onClick={()=>setColor("green")} className="outline-none px-4 rounded-full bg-green-600">Green</button>
+          <button onClick={()=>setColor("green")}  className="outline-none px-4 rounded-full bg-green-600">Green</button>
 
-          <button onClick={()=>setColor("Black")} className="outline-none px-4 rounded-full bg-black text-white ">Black</button >
+          <button onClick={()=>setColor("Black") } className="outline-none px-4 rounded-full bg-black text-white ">Black</button >
           <button onClick={() => setColor("red")} className="outline-none px-4 rounded-full bg-red-600">Red</button>
           <button onClick={() => setColor("blue")} className="outline-none px-4 rounded-full bg-blue-600">Blue</button>
           <button onClick={() => setColor("yellow")} className="outline-none px-4 rounded-full bg-yellow-600">Yellow</button>
